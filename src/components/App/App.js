b5eb53441033a2a32b459a895d0cba5dfd11d258
Joshua Nance <joshua.h.nance@gmail.com>
2021-07-08 18:51:55 -0500

    Add theme for exercise 1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import styled from 'styled-components/macro';
+import styled, { ThemeProvider } from 'styled-components/macro';
+import { BREAKPOINTS } from '../../constants';
 
 import Header from '../Header';
 import ShoeIndex from '../ShoeIndex';
+
+const theme = {
+  breakpoints: BREAKPOINTS,
+};
 
 const App = () => {
   const [sortId, setSortId] = React.useState('newest');
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       <Main>
         <ShoeIndex sortId={sortId} setSortId={setSortId} />
       </Main>
-    </>
+    </ThemeProvider>
   );
 };
 
--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,9 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+export const BREAKPOINTS = {
+  phone: 37.5,
+  tablet: 59.375,
+  laptop: 81.25,
+};
